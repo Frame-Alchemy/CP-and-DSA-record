162e91290c432262833bfdeebc09749531893d9c
Frame-Alchemy <framealchemy0@gmail.com>
2026-06-08 20:11:51 +0530

    900 01 game

--- Problemset/900/30_01game.cpp
+++ Problemset/900/30_01game.cpp
@@ -2,7 +2,14 @@
 using namespace std; 
 
 void solve(string s) {
-
+    int cnt1 = 0, cnt0 = 0;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == '1')cnt1++;
+        else cnt0++;
+    }
+    int min_moves = min(cnt1,cnt0);
+    if(min_moves%2 == 0) cout<<"NET\n";
+    else cout<<"DA\n";
 }
 
 int main() {
